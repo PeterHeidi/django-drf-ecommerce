1cec03484c45e681475b3c9297172e89d7bbdd0c
peterHeidi <spiderman@gmail.com>
2023-05-07 18:27:47 -0500

    To share the content to discord

--- drfecommerce/settings/base.py
+++ drfecommerce/settings/base.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    ### Frameworks
+    # Frameworks
     "rest_framework",
-    ### Apps
+    # Apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
